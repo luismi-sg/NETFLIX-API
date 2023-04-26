2ab4dcd3376b5e6c09d6445976ebbd4ec3fd8eff
Luis Miguel SG <lmsg1191@gmail.com>
2023-04-26 23:28:51 +0200

    Cambios en la API

--- index.js
+++ index.js
@@ -26,9 +26,11 @@ app.use( express.json() )
 app.use( express.urlencoded( {extended:false} ))
 app.use( router )
 
-// let bbdd = process.env.BBDD 
+//  let bbdd = process.env.BBDD
+let bbdd = 'mongodb+srv://lmsg1191:conectarmongo@cluster0.5jxyu5a.mongodb.net/NETFLIX' || 'mongodb://127.0.0.1:27017/NETFLIX'
+ 
 
-const main = async () => await mongoose.connect( 'mongodb+srv://lmsg1191:conectarmongo@cluster0.5jxyu5a.mongodb.net/NETFLIX' )
+const main = async () => await mongoose.connect( bddd )
             .then( () => console.log( `Conectando a MongoDB BBDD NETFLIX 👻`) )
 
 main() 
